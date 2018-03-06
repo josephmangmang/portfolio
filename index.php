6baaa59c930461f454b60c5d5ee9eaa297afaf3f
Joseph Mangmang <joseph.deplacer@gmail.com>
2018-03-06 16:35:08 +0800

    change dir structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<!--
+  Copyright © 2018 Joseph Mangmang
+ -->
+
+<?php 
+    require __DIR__ . '/layout/header.php';
+    require __DIR__ . '/layout/body.php';
+    require __DIR__ . '/layout/footer.php';
+ ?>
+   
